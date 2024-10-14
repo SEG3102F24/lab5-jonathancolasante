1491fcae2095b1d88554be3c81e8789a55b76f74
jonathancolasante <jonathan.colasante@gmail.com>
2024-10-13 20:14:57 -0400

    fix test

--- src/app/model/employee.spec.ts
+++ src/app/model/employee.spec.ts
@@ -2,6 +2,6 @@ import { Employee } from './employee';
 
 describe('Employee', () => {
   it('should create an instance', () => {
-    expect(new Employee("andrew", new Date(), "London", 1000)).toBeTruthy();
+    expect(new Employee("andrew", new Date(), "London", 1000, "", "")).toBeTruthy();
   });
 });
